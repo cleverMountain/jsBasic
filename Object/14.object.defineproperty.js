646f7da8482a277c2390cebbe0161a9b4b36dae2
Hjl <10319399225@>
2023-05-22 22:31:27 +0800

    proxy与defineproperty

--- Object/14.object.defineproperty.js
+++ Object/14.object.defineproperty.js
@@ -1,63 +1,94 @@
 function defineObj(obj) {
   // 需要一个对象来代理
-  let _obj = JSON.parse(JSON.stringify(obj))
+  let _obj = {}
   for(let k in obj ) {
-    Object.defineProperty(obj, k, {
-      get () {
-        console.log('get')
-        return _obj[k]
-      },
-      set(newVal) {
-        console.log('set')
-        _obj[k] = newVal
-      }
-    })
+    // console.log(obj[k])
+    const value = obj[k]
+    if (typeof value === 'object' && value !== null) {
+      _obj[k] = defineObj(value)
+    } else {
+      Object.defineProperty(_obj, k, {
+        get () {
+          console.log('get')
+          return obj[k]
+        },
+        set(newVal) {
+          console.log('set')
+          obj[k] = newVal
+        }
+      })
+    }
   }
 
-  return obj
+  return _obj
 }
 
 
 let obj = {
   a: 1,
-  b: 2
-}
-
-let obj1 = defineObj(obj)
-console.log(obj1)
-obj1.a = 3
-obj1.c = 10
-
-
-
-// 代理数组
-function defineArr (arr) {
- 
- 
-  function setDefin () {
-    let _arr = JSON.parse(JSON.stringify(arr))
-    
-    for(let k in arr) {
-      Object.defineProperty(arr, k, {
-        get() {
-          console.log('get arr')
-          return _arr[k]
-        },
-        set(newVal) {
-          console.log('set arr')
-          _arr[k] = newVal
-          debugger
-          setDefin(arr)
-        }
-      })
+  b: 2,
+  c: {
+    d: {
+      f: 1
     }
   }
-  setDefin()
-  return arr
 }
 
-let arr = [1, 2]
-let arr1 = defineArr(arr)
-console.log(arr1)
-// arr1[0] = 10
-arr1['2'] = 20
+let arr = [1,2]
+let obj1 = defineObj(obj)
+console.log(obj1)
+delete obj1.a
+// 缺点：1.单独增加或删除某个属性时，无法实现对该属性的监听
+//       2. 无法实现对数组的监听(数组是一个对象类型，是可以得到劫持后的数据，类似一个类数组，以0，1.。。为下标，但是无法使用push所有无法监听到) 
+
+
+
+const _obj = new Proxy(arr, {
+  get (target, prop, receive) {
+    console.log('proxyGet')
+    return target[prop]
+  },
+  set (target, prop, value, receive) {
+    console.log('proxySet')
+    return target[prop] = value
+  }
+})
+
+// _obj.a = 10
+// _obj.e = 11 // 单独添加某个属性特可以监听到
+
+console.log(_obj)
+setTimeout(() => {
+  _obj.push(3)
+}, 3000)
+// // 代理数组
+// function defineArr (arr) {
+ 
+ 
+//   function setDefin () {
+//     let _arr = JSON.parse(JSON.stringify(arr))
+    
+//     for(let k in arr) {
+//       Object.defineProperty(arr, k, {
+//         get() {
+//           console.log('get arr')
+//           return _arr[k]
+//         },
+//         set(newVal) {
+//           console.log('set arr')
+//           _arr[k] = newVal
+//           debugger
+//           setDefin(arr)
+//         }
+//       })
+//     }
+//   }
+//   setDefin()
+//   return arr
+// }
+
+// let arr = [1, 2]
+// let arr1 = defineArr(arr)
+// console.log(arr1)
+// // arr1[0] = 10
+// arr1['2'] = 20
--- Proxy_Reflect/4.Proxy.js
+++ Proxy_Reflect/4.Proxy.js
@@ -1,13 +1,105 @@
-let target = {
-  a: 1,
-  b: 2
-}
 
-let proxy = new Proxy(target, {
-  get () {
-    
-  },
-  set () {
+(function () {
 
+  class Computed {
+    constructor() {
+
+    }
+    compute() {
+      const { firstNumber, secondNumber, filed } = this.data
+      const one = Number(firstNumber) || 0
+      const two = Number(secondNumber) || 0
+      switch (filed) {
+        case '+':
+          this.resultEle.innerHTML = one + two
+          break
+        case '-':
+          this.resultEle.innerHTML = one - two
+          break
+        case '*':
+          this.resultEle.innerHTML = one * two
+          break
+        case '/':
+          this.resultEle.innerHTML = one / two
+          break
+      }
+    }
   }
-})
+
+  class Calculator extends Computed {
+    constructor() {
+      super()
+      this.resultEle = document.getElementsByClassName('result')[0]
+      this.firstNumEle = document.getElementsByClassName('f-input')[0]
+      this.secondNumEle = document.getElementsByClassName('s-input')[0]
+      this.buttonsEle = document.getElementsByClassName('button-group')[0]
+      this.data = this.definData()
+      this.init()
+    }
+    init() {
+      this.bindEvent()
+    }
+    definData() {
+      const _this = this
+      const data = {
+        firstNumber: 0,
+        secondNumber: 0,
+        filed: '+'
+      }
+
+      let obj = new Proxy(data, {
+        get(target, key, receiver) {
+          // receiver 源数据
+     
+         return  Reflect.get(target, key, receiver)
+          return target[key]
+        },
+        set(target, key, value,receiver) {
+         
+          // let res = target[key] = value
+          const res = Reflect.set(target, key, value, receiver)
+          _this.compute()
+          return res
+
+        }
+      })
+      return obj
+    }
+    bindEvent() {
+      this.firstNumEle.addEventListener('input', this.handFirstInput.bind(this), false)
+      this.secondNumEle.addEventListener('input', this.handSecondInput.bind(this), false)
+      this.buttonsEle.addEventListener('click', this.handClick.bind(this), false)
+    }
+    handFirstInput(ev) {
+      const e = ev || window.event
+      const target = e.target || e.srcElement
+      this.data.firstNumber = target.value
+    }
+    handSecondInput(ev) {
+      const e = ev || window.event
+      const target = e.target || e.srcElement
+      this.data.secondNumber = target.value
+    }
+    handClick(ev) {
+      const e = ev || window.event
+      const target = e.target || e.srcElement
+
+      const index = Array.prototype.indexOf.call(this.buttonsEle.children, target)
+      const current = this.buttonsEle.children[index]
+      this.changeClassName(index)
+      this.data.filed = current.innerHTML
+
+
+    }
+    changeClassName(index) {
+      const length = this.buttonsEle.children.length
+      for (let i = 0; i < length; i++) {
+        this.buttonsEle.children[i].className = 'button'
+      }
+      if (index == -1) return
+      this.buttonsEle.children[index].className += ' current'
+    }
+  }
+  let ca = new Calculator()
+  console.log(ca)
+})()
