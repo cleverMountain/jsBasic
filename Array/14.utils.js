baac70c7d0c0a1f3fd144f6b23c5a5c4b6735567
Hjl <10319399225@>
2023-05-22 19:09:45 +0800

    数组utils方法重新提交

---
+++ Array/14.utils.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @param {Array} arr 
+ * @param {String} prop 
+ * @returns 
+ */
+// 删除自身的重复值
+function removeSelfDuplicate(arr, prop) {
+  return arr.reduce((pre, item) => {
+    if (pre.every(ite => ite[prop] !== item[prop])) {
+      pre.push(item)
+    }
+    return pre
+  }, [])
+}
+
+
+
+
+/**
+ * 
+ * @param {Array} source  
+ * @param {Array} target  
+ * @param {String} prop   
+ * @param {Boolean} isDuplicate
+ * @returns 
+ */
+function getOtherDuplicate(source, target, prop, isDuplicate) {
+  return source.filter(s => {
+    let status = target.find(t => t[prop] == s[prop])
+    return isDuplicate ? status : !status
+  })
+}
