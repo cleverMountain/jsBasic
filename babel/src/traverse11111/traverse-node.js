f51a8a3416441cffc7c591af85a3edb656ce5f09
huangjunlin <9579711+huangjunlin511024@user.noreply.gitee.com>
2023-08-30 07:57:45 +0800

    babel

---
+++ babel/src/traverse11111/traverse-node.js
@@ -0,0 +1,25 @@
+
+exports.traverseNode = traverseNode;
+var _context = require("./context");
+var _t = require("@babel/types");
+const {
+  VISITOR_KEYS
+} = _t;
+function traverseNode(node, opts, scope, state, path, skipKeys, visitSelf) {
+  const keys = VISITOR_KEYS[node.type];
+  if (!keys) return false;
+  const context = new _context.default(scope, opts, state, path);
+  if (visitSelf) {
+    if (skipKeys != null && skipKeys[path.parentKey]) return false;
+    return context.visitQueue([path]);
+  }
+  for (const key of keys) {
+    if (skipKeys != null && skipKeys[key]) continue;
+    if (context.visit(node, key)) {
+      return true;
+    }
+  }
+  return false;
+}
+export default traverseNode
+//# sourceMappingURL=traverse-node.js.map
