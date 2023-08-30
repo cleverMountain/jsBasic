71f97955ce54c6e263755077035cd59925c75d5b
Hjl <1031939925@qq.com>
2023-08-30 16:52:57 +0800

    core

--- babel/src/index.js
+++ babel/src/index.js
@@ -1,44 +1,74 @@
-// @ts-nocheck
-// const parser = require('@babel/parser');
-// const traverse = require('@babel/traverse').default;
+// // @ts-nocheck
+// // const parser = require('@babel/parser');
+// // const traverse = require('@babel/traverse').default;
 
-// const code = `const message = 'Hello, World!'; console.log(message);`;
+// // const code = `const message = 'Hello, World!'; console.log(message);`;
 
-// const ast = parser.parse(code);
-// debugger
-// traverse(ast, {
+// // const ast = parser.parse(code);
+// // debugger
+// // traverse(ast, {
+// //   enter(path) {
+// //     if (path.isIdentifier({ name: 'message' })) {
+// //       debugger
+// //       path.node.name = 'newMessage';
+// //     }
+// //   },
+// // });
+
+// // console.log(ast);
+// const babel = require('@babel/core');
+// console.log(babel)
+
+// const code = `const message = '321'`;
+// // 1. 代码解析（Parse）
+// const ast = babel.parse(code);
+
+// // 2. AST 遍历（Traverse）
+// babel.traverse(ast, {
 //   enter(path) {
 //     if (path.isIdentifier({ name: 'message' })) {
-//       debugger
+
 //       path.node.name = 'newMessage';
 //     }
 //   },
 // });
+// console.log(11)
+// // 3. 代码转换（Transform）
+// const plugins = [/* 插件列表 */];
+// debugger
+// const transformedAst = babel.transform(ast);
 
-// console.log(ast);
-const babel = require('@babel/core');
-console.log(babel)
+// console.log(transformedAst)
+// debugger
+// // 4. 代码生成（Generate）
+// const transformedCode = babel.generate(transformedAst).code;
+// console.log(transformedCode)
+// debugger
+const babel = require("@babel/core");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const generator = require("@babel/generator").default;
 
-const code = `const message = 'Hello, World!'; console.log(message);`;
-// 1. 代码解析（Parse）
-const ast = babel.parse(code);
+// 要转换的代码
+const code = `
+  const message = "Hello, world!";
+  console.log(message);
+`;
 
-// 2. AST 遍历（Traverse）
-babel.traverse(ast, {
+// 解析（Parsing）
+const ast = parser.parse(code);
+
+debugger
+// 遍历（Traversing）
+traverse(ast, {
   enter(path) {
-    if (path.isIdentifier({ name: 'message' })) {
-
-      path.node.name = 'newMessage';
+    // 转换（Transformation）
+    if (path.isIdentifier({ name: "message" })) {
+      path.node.name = "newMessage";
     }
   },
 });
 
-// 3. 代码转换（Transform）
-const plugins = [/* 插件列表 */];
-const transformedAst = babel.transform(ast);
-console.log(transformedAst)
-debugger
-// 4. 代码生成（Generate）
-const transformedCode = babel.generate(transformedAst).code;
-console.log(transformedCode)
-debugger
+// 代码生成（Code Generation）
+const transformedCode = generator(ast).code;
+console.log(transformedCode);
