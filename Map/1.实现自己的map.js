393d1416701316de8c0e328b207232c47eabbd9a
huangjunlin <9579711+huangjunlin511024@user.noreply.gitee.com>
2023-09-18 08:07:00 +0800

    prompts

--- Map/1.实现自己的map.js
+++ Map/1.实现自己的map.js
@@ -1,43 +1,41 @@
-function MyMap() {
+function Map() {
   let index = 0
-  let arr = []
-  arr.set = function (key, value) {
-    arr[index++] = { key, value }
-  }
-  arr.get = function (key) {
-    return arr.find(item => item.key === key).value
-  }
-  arr.size = function () {
-    return arr.length
-  }
-  arr.has = function (key) {
-    return arr.find(item => item.key === key) ? true : false
-  }
-  arr.delete = function (key) {
-    if (arr.find(item => item.key === key)) {
-      let index = arr.findIndex(item => item.key === key)
-      arr.splice(index, 1)
-      return true
-    } else {
-      return '属性不存在'
-    }
-  }
-  arr.clear = function () {
-    arr.length = 0
-    return true
-  }
-  arr.keys = function () {
-    let newArr = this.filter(item => Object.prototype.toString.call(item) !== '[object Function]')
-    let len = newArr.length
-    let index = 0
-    newArr.next = function () {
-      return index < len ? newArr[index++].value : undefined
-    }
-    return newArr
-  }
-  return arr
+  this.map = {}
 }
-
+Map.prototype.set = function (key, value) {
+  this.arr[index++] = { key, value }
+}
+Map.prototype.get = function (key) {
+  return this.arr.find(item => item.key === key).value
+}
+Map.prototype.size = function () {
+  return this.arr.length
+}
+Map.prototype.has = function (key) {
+  return this.arr.find(item => item.key === key) ? true : false
+}
+arr.delete = function (key) {
+  if (arr.find(item => item.key === key)) {
+    let index = arr.findIndex(item => item.key === key)
+    arr.splice(index, 1)
+    return true
+  } else {
+    return '属性不存在'
+  }
+}
+arr.clear = function () {
+  arr.length = 0
+  return true
+}
+arr.keys = function () {
+  let newArr = this.filter(item => Object.prototype.toString.call(item) !== '[object Function]')
+  let len = newArr.length
+  let index = 0
+  newArr.next = function () {
+    return index < len ? newArr[index++].value : undefined
+  }
+  return newArr
+}
 let map = new MyMap()
 console.log(map)
 map.set('a', 1)
--- Object/4.Object.entries.js
+++ Object/4.Object.entries.js
@@ -1,18 +1,18 @@
-    // Object.entries()方法返回一个给定对象自身可枚举属性的键值对数组
-    // Object.entries(obj)
-    console.log(Object.entries({ a: 1, b: 2 })) // [['a', 1], ['b', 2]]
+// Object.entries()方法返回一个给定对象自身可枚举属性的键值对数组
+// Object.entries(obj)
+console.log(Object.entries({ a: 1, b: 2 })) // [['a', 1], ['b', 2]]
 
-    Object.prototype.myEntries = function (obj) {
-      let arr = []
-      // for in 会枚举原型链中的属性
-      for (let k in obj) {
-        if (obj.hasOwnProperty(k)) {
-          arr.push([k, obj[k]])
-        }
-      }
-      return arr
+Object.prototype.myEntries = function (obj) {
+  let arr = []
+  // for in 会枚举原型链中的属性
+  for (let k in obj) {
+    if (obj.hasOwnProperty(k)) {
+      arr.push([k, obj[k]])
     }
-    let d = Object.myEntries({ a: 1, b: 2 })
+  }
+  return arr
+}
+let d = Object.myEntries({ a: 1, b: 2 })
 
-    let map = new Map(d);
-    console.log(map)
+let map = new Map(d);
+console.log(map)
